196988d7f41f373f62510e63567102f49e4cf139
frarav <frarav@test02.sindice.net>
2013-03-29 13:36:17 +0000

    Created the scrapy crunch project

---
+++ crunch/crunch/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for crunch project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'crunch'
+
+SPIDER_MODULES = ['crunch.spiders']
+NEWSPIDER_MODULE = 'crunch.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'crunch (+http://www.yourdomain.com)'
